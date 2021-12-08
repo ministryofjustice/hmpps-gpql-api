cbe672df6f9a5485b0eaf48c08af6604a52fde25
Mjwillis <Mjwillis@users.noreply.github.com>
2021-12-08 15:35:31 +0000

    updating project name after deployment from template repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/gqlapi/GqlApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/gqlapi/GqlApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename
+package uk.gov.justice.digital.hmpps.gqlapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
-class HmppsTemplateKotlin
+class GqlApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<GqlApi>(*args)
 }
