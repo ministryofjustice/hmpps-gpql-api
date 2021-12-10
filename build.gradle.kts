dd2d7d6be2ce1d33578e1d364473e96cde8ee359
Michael Willis <michael.willis@digital.justice.gov.uk>
2021-12-10 16:14:17 +0000

    DT-2897 Add links to prison and communitiy api

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,22 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.experimental:graphql-spring-boot-starter:1.0.0-M3")
+
+  implementation("org.apache.commons:commons-lang3:3.12.0")
+  implementation("org.apache.commons:commons-text:1.9")
+  implementation("commons-codec:commons-codec:1.15")
+
+  implementation("com.pauldijou:jwt-core_2.11:5.0.0")
+
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+
+
 }
 
 java {
